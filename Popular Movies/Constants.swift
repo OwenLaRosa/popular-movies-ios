3f345736ef9cc8aeddde114d4cdd7d4f269b6651
OwenLaRosa <owen.larosa@outlook.com>
2016-06-24 16:51:45 -0400

    Add model class for movies

---
+++ Popular Movies/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Popular Movies
+//
+//  Created by Owen LaRosa on 6/24/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+
+public struct Constants {
+    
+    public struct PosterConstants {
+        static let posterBasePath = "http://image.tmdb.org/t/p/"
+        static let imageSize = "w185"
+    }
+    
+}
---
+++ Popular Movies/MovieModel.swift
@@ -0,0 +1,35 @@
+//
+//  MovieModel.swift
+//  Popular Movies
+//
+//  Created by Owen LaRosa on 6/24/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+
+public class Movie {
+    
+    public let id: Int
+    public let title: String
+    public let posterPath: String
+    public let releaseDate: String
+    public let rating: Int
+    public let overview: String
+    
+    public init(properties: [String: AnyObject]) {
+        id = properties[""] as! Int
+        title = properties[""] as! String
+        posterPath = properties[""] as! String
+        releaseDate = properties[""] as! String
+        rating = properties[""] as! Int
+        overview = properties[""] as! String
+    }
+    
+    public func getFullPosterPath() -> String {
+        return "\(Constants.PosterConstants.posterBasePath)\(Constants.PosterConstants.imageSize)/\(posterPath)"
+    }
+    
+}
+
+
