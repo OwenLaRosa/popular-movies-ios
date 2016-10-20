956009b60caa37130b37c2862784cbf57efedb3c
OwenLaRosa <owen.larosa@outlook.com>
2016-10-20 10:34:00 -0400

    feat: Add cell for trailer

---
+++ Popular Movies/TrailerCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrailerCollectionViewCell.swift
+//  Popular Movies
+//
+//  Created by Owen LaRosa on 10/20/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class TrailerCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    
+}
